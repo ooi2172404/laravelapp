b8e9534d85d6c8da3a74210e60ee16e2a1f8d3f2
Tomoyuki-Okuma <bizarre.dept@gmail.com>
2022-07-01 14:46:00 +0900

    merge

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
 
+<<<<<<< HEAD
 
 use Illuminate\Support\Facades\Route;
 //問題1
@@ -26,3 +27,13 @@ Route::get('kouka1_1', 'Kouka1_1Controller@index');
 //問題2
 Route::get('___(1)___', '___(2)___');
 Route::post('___(3)___', '___(4)___');
+=======
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+>>>>>>> 69babed17711604eb3a1bd14fc7ea5918b1a245c

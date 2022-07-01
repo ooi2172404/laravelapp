56dc9211472049e28c3ca79c6305bf419c77c7d4
Tomoyuki-Okuma <bizarre.dept@gmail.com>
2022-07-01 12:17:57 +0900

    テキストP193 パラメータ結合

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,11 +12,15 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        if (isset($request->id)) {
+            $param = ['id' => $request->id];
+            $items = DB::select(
+                'select * from people where id = :id',
+                $param
+            );
+        } else {
+            $items = DB::select('select * from people');
+        }
         return view('hello.index', ['items' => $items]);
-    }
-    public function post(HelloRequest $request)
-    {
-        return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }

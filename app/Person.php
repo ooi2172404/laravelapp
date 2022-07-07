80312f729c09b2d17840cb1e05feeb77902823bc
Tomoyuki-Okuma <bizarre.dept@gmail.com>
2022-07-07 14:54:16 +0900

    テキストP280まで

--- app/Person.php
+++ app/Person.php
@@ -21,4 +21,8 @@ class Person extends Model
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
+    public function boards()
+    {
+        return $this->hasMany('App\Board');
+    }
 }

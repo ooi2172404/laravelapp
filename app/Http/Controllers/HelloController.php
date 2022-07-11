44f9f49047ad1facea48f7ee99da8b3cab2f2b94
Tomoyuki-Okuma <bizarre.dept@gmail.com>
2022-07-11 14:45:46 +0900

    aaaa

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,19 +7,21 @@ use App\Http\Requests\HelloRequest;
 use Validator;
 use Illuminate\Support\Facades\DB;
 use App\Person;
+use Illuminate\Support\Facades\Auth;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
+        $user = Auth::user();
         if (isset($request->sort)) {
             $sort = $request->sort;
         } else {
             $sort = 'age';
         }
         $items = Person::orderBy($sort, 'asc')
-            ->simplePaginate(5);
-        $param = ['items' => $items, 'sort' => $sort];
+            ->paginate(5);
+        $param = ['items' => $items, 'sort' => $sort, 'user' => $user];
         return view('hello.index', $param);
     }
     public function post(Request $request)
@@ -101,5 +103,25 @@ class HelloController extends Controller
         $msg = $request->input;
         $request->session()->put('msg', $msg);
         return redirect('hello/session');
+    }
+    public function getAuth(Request $request)
+    {
+        $param = ['message' => 'ログインして下さい。'];
+        return view('hello.auth', $param);
+    }
+
+    public function postAuth(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        if (Auth::attempt([
+            'email' => $email,
+            'password' => $password
+        ])) {
+            $msg = 'ログインしました。（' . Auth::user()->name . '）';
+        } else {
+            $msg = 'ログインに失敗しました。';
+        }
+        return view('hello.auth', ['message' => $msg]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', 'HelloController@index')
+    ->middleware('auth');
 Route::post('hello', 'HelloController@post');
 
 Route::get('hello/add', 'HelloController@add');
@@ -55,3 +56,10 @@ Route::get('hello/rest', 'HelloController@rest');
 
 Route::get('hello/session', 'HelloController@ses_get');
 Route::post('hello/session', 'HelloController@ses_put');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('hello/auth', 'HelloController@getAuth');
+Route::post('hello/auth', 'HelloController@postAuth');
